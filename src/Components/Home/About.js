dd4b9c51032f509f11c82949023971e90e1983b4
Al-aminhossain1 <md.al.aminhossain1785@gmail.com>
2022-06-09 17:38:34 +0600

    added homepage

---
+++ src/Components/Home/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className='bg-black'>
+            <h1 className='text-center text-4xl font-medium text-cyan-400'>About Me</h1>
+
+
+        </div>
+    );
+};
+
+export default About;
